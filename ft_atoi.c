e2eaf3c8958b5bbdae58c72a4274a5cbfb7e862f
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-03-30 01:11:08 +0400

    Update ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -21,7 +21,7 @@ int	ft_atoi(const	char *nptr)
 	sign = 1;    
 	result = 0;   
 	while (nptr && ((*nptr >= 9 && *nptr <= 13) || *nptr == 32))
-		nptr++;
+		nptr++; 
 	if (*nptr == '-' || *nptr == '+')
 	{  
 		if (*nptr == '-')
