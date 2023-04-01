c6176b86c6edae4f38b4af2c2f2a1060a9963f55
Simon Zerisenay <81813787+simon-zerisenay@users.noreply.github.com>
2023-04-02 02:15:35 +0400

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -9,7 +9,7 @@
 /*   Updated: 2023/01/04 15:01:23 by szerisen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
- 
+  
 #include "libft.h" 
 
 void	*ft_calloc(size_t count, size_t size)  
